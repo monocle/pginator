d5f758fae46a2c334d456a495f82eeabe14f508a
monocle <monocle.github@gmail.com>
2023-04-09 14:07:56 -0700

    Update db.py
    - Use ConnectionPool
    - Fix drop_all_tables to drop tables synchronously

--- backend/src/app/db.py
+++ backend/src/app/db.py
@@ -1,20 +1,22 @@
-from typing import List, Optional
+from contextlib import _GeneratorContextManager
+from typing import Any, List, Optional
 
-import psycopg as pg
 from flask import Flask, current_app
 from psycopg.abc import Params, Query
 from psycopg.connection import Connection
 from psycopg.cursor import Cursor
-from psycopg.rows import DictRow, TupleRow, dict_row
+from psycopg.rows import DictRow, dict_row
+from psycopg_pool import ConnectionPool
 
 
 class DB:
     def __init__(self, app: Optional[Flask] = None):
         _app = app if app else current_app
         self.uri = _app.config["DB_URI"]
+        self.connection_pool = ConnectionPool(self.uri)
 
-    def connect(self) -> Connection[TupleRow]:
-        return pg.connect(self.uri)
+    def connect(self) -> _GeneratorContextManager[Connection[Any]]:
+        return self.connection_pool.connection()
 
     def execute(self, sql: Query, params: Params | None = None) -> Cursor[DictRow]:
         with self.connect() as conn:
@@ -33,8 +35,11 @@ class DB:
 
     def drop_all_tables(self) -> None:
         sql = """SELECT 'DROP TABLE IF EXISTS "' || tablename || '" CASCADE;' FROM pg_tables WHERE schemaname = 'public';"""
+        statements = []
 
         with self.connect() as conn:
-            with conn.cursor() as cursor:
-                for statement in cursor.execute(sql):
-                    cursor.execute(statement[0])
+            statements = conn.execute(sql).fetchall()
+
+        with self.connect() as conn:
+            for statement in statements:
+                conn.execute(statement[0])
