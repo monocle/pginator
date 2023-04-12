a0bb29e717436548bb8aa38f990501afb28e7c54
monocle <monocle.github@gmail.com>
2023-04-12 10:05:37 -0700

    Add remaining CRUD operations for Row

--- backend/src/app/models/row.py
+++ backend/src/app/models/row.py
@@ -42,9 +42,15 @@ SELECT_ROWS_SQL = SQL(
     """
 )
 
+FIND_ROW_SQL = SQL("SELECT * FROM {table_name} WHERE id = {id}")
+
 INSERT_ROWS_SQL = SQL(
     "INSERT INTO {table_name} ({columns}) VALUES ({values}) RETURNING *"
 )
+
+UPDATE_ROW_SQL = SQL("UPDATE {table_name} SET {columns} WHERE id = {id} RETURNING *")
+
+DELETE_ROW_SQL = SQL("DELETE FROM {table_name} WHERE id = {id} RETURNING id")
 
 
 class QueryParams(BaseModel):
@@ -58,22 +64,21 @@ class QueryParams(BaseModel):
 
 class Row:
     def __init__(
-        self, table_name: str, request_dict: dict | None = None, params_dict=None
+        self,
+        table_name: str,
+        id: str | None = None,
+        request_dict: dict | None = None,
+        query_params_dict=None,
     ) -> None:
+        self.id = id
         self.request_dict = request_dict
-        self.params_dict = params_dict
+        self.request_keys = tuple(request_dict.keys() if request_dict else [])
+        self.request_values = tuple(request_dict.values() if request_dict else [])
+        self.query_params_dict = query_params_dict
         self.table_name = table_name
         self._errors: list["ErrorDict"] = []
 
-        self.validate()
-
-    def validate(self) -> None:
-        if not self.table_name:
-            self._add_error(("table_name",), "field required")
-        try:
-            self.params = QueryParams(**self.params_dict or {})
-        except ValidationError as e:
-            self._errors = e.errors()
+        self._validate()  # self.query_params set here
 
     def all(self) -> list[DictRow] | None:
         if not self.valid:
@@ -82,26 +87,58 @@ class Row:
         sql = SELECT_ROWS_SQL.format(
             name=Identifier(self.table_name),
             name_str=self.table_name,
-            limit=self.params.limit,
-            offset=self.params.offset,
+            limit=self.query_params.limit,
+            offset=self.query_params.offset,
         )
         return DB().fetch_all(sql)
 
-    def create(self) -> DictRow | None:
-        # The seecond check is for type checker.
-        if not self._valid_column_names() or not self.request_dict:
+    def find(self) -> DictRow | None:
+        if not self.id:
             return None
 
-        columns = [Identifier(col) for col in self.request_dict.keys()]
-        placeholders = [Placeholder() for _ in self.request_dict.keys()]
-        values = tuple(self.request_dict.values())
+        sql = FIND_ROW_SQL.format(
+            table_name=Identifier(self.table_name),
+            id=self.id,
+        )
+        return DB().fetch_one(sql)
+
+    def create(self) -> DictRow | None:
+        if not self.valid:
+            return None
+
+        columns = [Identifier(col) for col in self.request_keys]
 
         sql = INSERT_ROWS_SQL.format(
             table_name=Identifier(self.table_name),
             columns=SQL(", ").join(columns),
-            values=SQL(", ").join(values),  # TODO is this necessary?
+            values=SQL(", ").join(self.request_values),
         )
+        return DB().fetch_one(sql)
 
+    def update(self) -> DictRow | None:
+        if not self.valid or not self.id:
+            return None
+
+        columns = [
+            SQL("{} = {}").format(Identifier(col), Placeholder())
+            for col in self.request_keys
+        ]
+
+        sql = UPDATE_ROW_SQL.format(
+            table_name=Identifier(self.table_name),
+            columns=SQL(", ").join(columns),
+            id=self.id,
+        )
+        return DB().fetch_one(sql, self.request_values)
+
+    def delete(self) -> DictRow | None:
+        if not self.id:
+            return None
+
+        sql = DELETE_ROW_SQL.format(
+            table_name=Identifier(self.table_name),
+            id=self.id,
+        )
         return DB().fetch_one(sql)
 
     @property
@@ -112,7 +149,19 @@ class Row:
     def errors(self) -> tuple["ErrorDict", ...]:
         return tuple(self._errors)
 
-    def _valid_column_names(self) -> bool:
+    def _validate(self) -> None:
+        if not self.table_name:
+            self._add_error(("table_name",), "field required")
+
+        try:
+            self.query_params = QueryParams(**self.query_params_dict or {})
+        except ValidationError as e:
+            self._errors = e.errors()
+
+        if self.request_dict is not None:
+            self._validate_column_names()
+
+    def _validate_column_names(self) -> bool:
         if not self.request_dict:
             self._add_error(
                 ("column",),
@@ -120,12 +169,14 @@ class Row:
             )
             return False
 
+        # Is this check necessary since we're just passing all Postges errors back
+        # to the client anyways? Is security improved?
         allowed_columns = {
             row["column_name"]
             for row in DB().fetch_all(SELECT_TABLE_COLUMNS_SQL, (self.table_name,))
         }
 
-        input_columns: set[str] = set(self.request_dict.keys())
+        input_columns: set[str] = set(self.request_keys)
         extra_columns = input_columns.difference(allowed_columns)
 
         if extra_columns:
--- backend/src/tests/models/test_row.py
+++ backend/src/tests/models/test_row.py
@@ -25,18 +25,52 @@ def app(app_factory):
 
 @pytest.mark.parametrize(
     "params, expected_count",
-    [({}, 20), ({"limit": 2, "offset": 2}, 2), ({"limit": 2, "offset": 22}, 0)],
+    [
+        ({}, 20),
+        ({"limit": 2, "offset": 2}, 2),
+        ({"limit": 2, "offset": 22}, 0),
+        ({"limit": -1}, None),
+    ],
 )
 def test_row_all(app, params, expected_count):
-    offset = params.get("offset", 0)
-    last_idx = expected_count - 1
-    row = Row(TABLE_NAME, params_dict=params)
-    results = row.all() or []
+    row = Row(TABLE_NAME, query_params_dict=params)
+    results = row.all()
 
-    assert len(results) == expected_count
+    if expected_count is None:
+        assert results is None
+    else:
+        offset = params.get("offset", 0)
+        last_idx = expected_count - 1
 
-    if expected_count:
-        assert results[last_idx]["first_name"] == f"first_name{last_idx+ offset}"
+        assert results is not None
+        assert len(results) == expected_count
+
+        if expected_count:
+            assert results[last_idx]["first_name"] == f"first_name{last_idx+ offset}"
+
+
+@pytest.mark.parametrize(
+    "row_id,expected_result",
+    [
+        (
+            "1",
+            {
+                "id": 1,
+                "first_name": "first_name0",
+                "last_name": "last_name0",
+            },
+        ),
+        (None, None),
+    ],
+)
+def test_row_find(app, row_id, expected_result):
+    row = Row(TABLE_NAME, id=row_id)
+    found_row = row.find()
+
+    if expected_result is None:
+        assert found_row is None
+    else:
+        assert found_row == expected_result
 
 
 @pytest.mark.parametrize(
@@ -52,6 +86,7 @@ def test_row_all(app, params, expected_count):
             {"first_name": "foo", "last_name": "bar@#$%^&*()_+="},
             {"first_name": "foo", "last_name": "bar@#$%^&*()_+=", "id": 22},
         ),
+        ({}, None),
     ],
 )
 def test_row_create(app, request_dict, expected_result):
@@ -64,6 +99,48 @@ def test_row_create(app, request_dict, expected_result):
         assert res
         for key, value in expected_result.items():
             assert res.get(key) == value
+
+
+@pytest.mark.parametrize(
+    "request_dict, expected_result",
+    [
+        (
+            {"first_name": "updated value"},
+            {"id": 1, "first_name": "updated value", "last_name": "last_name0"},
+        ),
+        (
+            {"first_name": "updated value", "last_name": "another updated value"},
+            {
+                "id": 1,
+                "first_name": "updated value",
+                "last_name": "another updated value",
+            },
+        ),
+        ({}, None),
+    ],
+)
+def test_row_update(app, request_dict: dict, expected_result: dict):
+    row = Row(TABLE_NAME, request_dict=request_dict, id="1")
+    res = row.update()
+
+    assert res == expected_result
+
+
+@pytest.mark.parametrize(
+    "row_id, expected_result",
+    [
+        ("1", {"id": 1}),
+        (None, None),
+    ],
+)
+def test_row_delete(app, row_id, expected_result):
+    row = Row(TABLE_NAME, id=row_id)
+    res = row.delete()
+
+    assert res == expected_result
+
+    if row_id is not None:
+        assert row.find() is None
 
 
 def test_row_table_name_validation(app):
@@ -80,11 +157,77 @@ def test_row_table_name_validation(app):
 
 
 @pytest.mark.parametrize(
+    "query_params_dict, expected_errors",
+    [
+        (
+            {"limit": -1},
+            (
+                {
+                    "ctx": {"limit_value": 0},
+                    "loc": ("limit",),
+                    "msg": "ensure this value is greater than 0",
+                    "type": "value_error.number.not_gt",
+                },
+            ),
+        ),
+        (
+            {"offset": -1},
+            (
+                {
+                    "ctx": {"limit_value": 0},
+                    "loc": ("offset",),
+                    "msg": "ensure this value is greater than or equal to 0",
+                    "type": "value_error.number.not_ge",
+                },
+            ),
+        ),
+        (
+            {"limit": -1, "offset": -1},
+            (
+                {
+                    "loc": ("limit",),
+                    "msg": "ensure this value is greater than 0",
+                    "type": "value_error.number.not_gt",
+                    "ctx": {"limit_value": 0},
+                },
+                {
+                    "loc": ("offset",),
+                    "msg": "ensure this value is greater than or equal to 0",
+                    "type": "value_error.number.not_ge",
+                    "ctx": {"limit_value": 0},
+                },
+            ),
+        ),
+        ({"limit": 1}, ()),
+        ({"offset": 0}, ()),
+    ],
+)
+def test_row_query_params_validation(app, query_params_dict, expected_errors):
+    row = Row(
+        TABLE_NAME,
+        query_params_dict=query_params_dict,
+    )
+
+    if expected_errors:
+        assert not row.valid
+    else:
+        assert row.valid
+
+    assert row.errors == expected_errors
+
+
+@pytest.mark.parametrize(
     "request_dict, expected_errors",
     [
         (
-            {"first_name": "foo", "last_name": "bar"},
-            (),
+            {"invalid_column": "test_value"},
+            (
+                {
+                    "loc": ("invalid_column",),
+                    "msg": 'Invalid columns for table "users"',
+                    "type": "value_error.invalid",
+                },
+            ),
         ),
         (
             {},
@@ -96,22 +239,16 @@ def test_row_table_name_validation(app):
                 },
             ),
         ),
-        (
-            {"first_name": "foo", "doesnt_exist": "bar"},
-            (
-                {
-                    "loc": ("doesnt_exist",),
-                    "msg": f'Invalid columns for table "{TABLE_NAME}"',
-                    "type": "value_error.invalid",
-                },
-            ),
-        ),
     ],
 )
-def test_row_request_dict_validation(app, request_dict, expected_errors):
+def test_row_invalid_column_names(app, request_dict, expected_errors):
     row = Row(TABLE_NAME, request_dict=request_dict)
 
-    row.create()
+    if expected_errors:
+        assert not row.valid
+    else:
+        assert row.valid
+
     assert row.errors == expected_errors
 
 
